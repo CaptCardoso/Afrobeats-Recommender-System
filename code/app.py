396096b5b82b79ef2f69842daf2854c3db79761e
Cardoso Afolabi <cardosoafolabi@yahoo.com>
2022-04-04 18:19:38 -0400

    'add title'

--- code/app.py
+++ code/app.py
@@ -2,26 +2,29 @@ from operator import mod
 import streamlit as st
 import pickle
 
+st.title('Afrobeats Recommendation System')
+
+
 # @st.cache
-def load_model():
-  with open('models/author_pipe.pkl', 'rb') as f:
-    the_model = pickle.load(f)
-  return the_model
+# def load_model():
+#   with open('models/author_pipe.pkl', 'rb') as f:
+#     the_model = pickle.load(f)
+#   return the_model
 
-model = load_model()
+# model = load_model()
 
-st.title('Which author is your muse?')
+# st.title('Which author is your muse?')
 
-st.subheader('Do you write like Jane Austen or Edgar Allan Poe?')
+# st.subheader('Do you write like Jane Austen or Edgar Allan Poe?')
 
-txt = st.text_area('Write your prose here').strip()
+# txt = st.text_area('Write your prose here').strip()
 
-if st.button('Submit'):
-  if len(txt) > 0:
-    pred = model.predict([txt])[0]
-    probs = list(model.predict_proba([txt])[0])
-    prob = probs[0] if pred == 'Edgar Allan Poe' else probs[1]
-    st.write('You write like ', pred)
-    st.metric('Probability', f'{100 * round(prob, 2)}%')
-  else:
-    st.write('Too pithy. Try writing something.')
+# if st.button('Submit'):
+#   if len(txt) > 0:
+#     pred = model.predict([txt])[0]
+#     probs = list(model.predict_proba([txt])[0])
+#     prob = probs[0] if pred == 'Edgar Allan Poe' else probs[1]
+#     st.write('You write like ', pred)
+#     st.metric('Probability', f'{100 * round(prob, 2)}%')
+#   else:
+#     st.write('Too pithy. Try writing something.')
